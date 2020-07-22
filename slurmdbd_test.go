399cb4f22d53563a7a2ff2be054e666a5ed2f72a
Trey Dockendorf <tdockendorf@osc.edu>
2020-07-22 11:59:48 -0400

    Add slurmdbd collector that executes 'sacctmgr show stats'

---
+++ slurmdbd_test.go
@@ -0,0 +1,49 @@
+/* Copyright 2017 Victor Penso, Matteo Dessalvi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/go-kit/kit/log"
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+func TestSlurmdbdMetrics(t *testing.T) {
+	// Read the input data from a file
+	file, err := os.Open("test_data/sacctmgr_stats.txt")
+	if err != nil {
+		t.Fatalf("Can not open test data: %v", err)
+	}
+	data, err := ioutil.ReadAll(file)
+	w := log.NewSyncWriter(os.Stderr)
+	logger := log.NewLogfmtLogger(w)
+	t.Logf("%+v", ParseSlurmdbdMetrics(string(data), logger))
+}
+
+func TestSlurmdbdGetMetrics(t *testing.T) {
+	if _, err := kingpin.CommandLine.Parse([]string{}); err != nil {
+		t.Fatal(err)
+	}
+	timeout := 10
+	collectorTimeout = &timeout
+	w := log.NewSyncWriter(os.Stderr)
+	logger := log.NewLogfmtLogger(w)
+	data, _ := SlurmdbdGetMetrics(logger)
+	t.Logf("%+v", data)
+}
