9d0258ef543d05d6756db566ffaea7ecc6e33327
Trey Dockendorf <tdockendorf@osc.edu>
2020-10-02 14:31:32 -0400

    Add slurm_queue_pending_reason_count metric

--- queue.go
+++ queue.go
@@ -28,19 +28,20 @@ import (
 )
 
 type QueueMetrics struct {
-	total       float64
-	pending     float64
-	pending_dep float64
-	running     float64
-	suspended   float64
-	cancelled   float64
-	completing  float64
-	completed   float64
-	configuring float64
-	failed      float64
-	timeout     float64
-	preempted   float64
-	node_fail   float64
+	total           float64
+	pending         float64
+	pending_dep     float64
+	pending_reasons map[string]float64
+	running         float64
+	suspended       float64
+	cancelled       float64
+	completing      float64
+	completed       float64
+	configuring     float64
+	failed          float64
+	timeout         float64
+	preempted       float64
+	node_fail       float64
 }
 
 // Returns the scheduler metrics
@@ -54,6 +55,7 @@ func QueueGetMetrics(logger log.Logger) (*QueueMetrics, error) {
 
 func ParseQueueMetrics(input string) *QueueMetrics {
 	var qm QueueMetrics
+	pr := make(map[string]float64)
 	lines := strings.Split(string(input), "\n")
 	for _, line := range lines {
 		if strings.Contains(line, ",") {
@@ -63,8 +65,11 @@ func ParseQueueMetrics(input string) *QueueMetrics {
 			switch state {
 			case "PENDING":
 				qm.pending++
-				if len(splitted) > 2 && splitted[2] == "Dependency" {
-					qm.pending_dep++
+				if len(splitted) > 2 {
+					pr[splitted[2]] += 1
+					if splitted[2] == "Dependency" {
+						qm.pending_dep++
+					}
 				}
 			case "RUNNING":
 				qm.running++
@@ -89,6 +94,7 @@ func ParseQueueMetrics(input string) *QueueMetrics {
 			}
 		}
 	}
+	qm.pending_reasons = pr
 	return &qm
 }
 
@@ -121,44 +127,47 @@ func QueueData(logger log.Logger) (string, error) {
 
 func NewQueueCollector(logger log.Logger) *QueueCollector {
 	return &QueueCollector{
-		total:       prometheus.NewDesc("slurm_queue_total", "Total jobs in queue", nil, nil),
-		pending:     prometheus.NewDesc("slurm_queue_pending", "Pending jobs in queue", nil, nil),
-		pending_dep: prometheus.NewDesc("slurm_queue_pending_dependency", "Pending jobs because of dependency in queue", nil, nil),
-		running:     prometheus.NewDesc("slurm_queue_running", "Running jobs in the cluster", nil, nil),
-		suspended:   prometheus.NewDesc("slurm_queue_suspended", "Suspended jobs in the cluster", nil, nil),
-		cancelled:   prometheus.NewDesc("slurm_queue_cancelled", "Cancelled jobs in the cluster", nil, nil),
-		completing:  prometheus.NewDesc("slurm_queue_completing", "Completing jobs in the cluster", nil, nil),
-		completed:   prometheus.NewDesc("slurm_queue_completed", "Completed jobs in the cluster", nil, nil),
-		configuring: prometheus.NewDesc("slurm_queue_configuring", "Configuring jobs in the cluster", nil, nil),
-		failed:      prometheus.NewDesc("slurm_queue_failed", "Number of failed jobs", nil, nil),
-		timeout:     prometheus.NewDesc("slurm_queue_timeout", "Jobs stopped by timeout", nil, nil),
-		preempted:   prometheus.NewDesc("slurm_queue_preempted", "Number of preempted jobs", nil, nil),
-		node_fail:   prometheus.NewDesc("slurm_queue_node_fail", "Number of jobs stopped due to node fail", nil, nil),
-		logger:      log.With(logger, "collector", "queue"),
+		total:           prometheus.NewDesc("slurm_queue_total", "Total jobs in queue", nil, nil),
+		pending:         prometheus.NewDesc("slurm_queue_pending", "Pending jobs in queue", nil, nil),
+		pending_dep:     prometheus.NewDesc("slurm_queue_pending_dependency", "Pending jobs because of dependency in queue", nil, nil),
+		pending_reasons: prometheus.NewDesc("slurm_queue_pending_reason_count", "Count of reasons for pending jobs", []string{"reason"}, nil),
+		running:         prometheus.NewDesc("slurm_queue_running", "Running jobs in the cluster", nil, nil),
+		suspended:       prometheus.NewDesc("slurm_queue_suspended", "Suspended jobs in the cluster", nil, nil),
+		cancelled:       prometheus.NewDesc("slurm_queue_cancelled", "Cancelled jobs in the cluster", nil, nil),
+		completing:      prometheus.NewDesc("slurm_queue_completing", "Completing jobs in the cluster", nil, nil),
+		completed:       prometheus.NewDesc("slurm_queue_completed", "Completed jobs in the cluster", nil, nil),
+		configuring:     prometheus.NewDesc("slurm_queue_configuring", "Configuring jobs in the cluster", nil, nil),
+		failed:          prometheus.NewDesc("slurm_queue_failed", "Number of failed jobs", nil, nil),
+		timeout:         prometheus.NewDesc("slurm_queue_timeout", "Jobs stopped by timeout", nil, nil),
+		preempted:       prometheus.NewDesc("slurm_queue_preempted", "Number of preempted jobs", nil, nil),
+		node_fail:       prometheus.NewDesc("slurm_queue_node_fail", "Number of jobs stopped due to node fail", nil, nil),
+		logger:          log.With(logger, "collector", "queue"),
 	}
 }
 
 type QueueCollector struct {
-	total       *prometheus.Desc
-	pending     *prometheus.Desc
-	pending_dep *prometheus.Desc
-	running     *prometheus.Desc
-	suspended   *prometheus.Desc
-	cancelled   *prometheus.Desc
-	completing  *prometheus.Desc
-	completed   *prometheus.Desc
-	configuring *prometheus.Desc
-	failed      *prometheus.Desc
-	timeout     *prometheus.Desc
-	preempted   *prometheus.Desc
-	node_fail   *prometheus.Desc
-	logger      log.Logger
+	total           *prometheus.Desc
+	pending         *prometheus.Desc
+	pending_dep     *prometheus.Desc
+	pending_reasons *prometheus.Desc
+	running         *prometheus.Desc
+	suspended       *prometheus.Desc
+	cancelled       *prometheus.Desc
+	completing      *prometheus.Desc
+	completed       *prometheus.Desc
+	configuring     *prometheus.Desc
+	failed          *prometheus.Desc
+	timeout         *prometheus.Desc
+	preempted       *prometheus.Desc
+	node_fail       *prometheus.Desc
+	logger          log.Logger
 }
 
 func (qc *QueueCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- qc.total
 	ch <- qc.pending
 	ch <- qc.pending_dep
+	ch <- qc.pending_reasons
 	ch <- qc.running
 	ch <- qc.suspended
 	ch <- qc.cancelled
@@ -183,6 +192,9 @@ func (qc *QueueCollector) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(qc.total, prometheus.GaugeValue, qm.total)
 	ch <- prometheus.MustNewConstMetric(qc.pending, prometheus.GaugeValue, qm.pending)
 	ch <- prometheus.MustNewConstMetric(qc.pending_dep, prometheus.GaugeValue, qm.pending_dep)
+	for reason, count := range qm.pending_reasons {
+		ch <- prometheus.MustNewConstMetric(qc.pending_reasons, prometheus.GaugeValue, count, reason)
+	}
 	ch <- prometheus.MustNewConstMetric(qc.running, prometheus.GaugeValue, qm.running)
 	ch <- prometheus.MustNewConstMetric(qc.suspended, prometheus.GaugeValue, qm.suspended)
 	ch <- prometheus.MustNewConstMetric(qc.cancelled, prometheus.GaugeValue, qm.cancelled)
